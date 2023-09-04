c33d5bc8af0740381b55b3040b349846d1e22f81
Luis Acevedo <115041872+SrEsiuL@users.noreply.github.com>
2023-09-04 09:26:15 -0500

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,10 @@
 function guardar(){
-    if (document.getElementById("nomb").value != ''){
-            var _nom = document.getElementById("nomb").value;
-            if (document.getElementById("age").value != ''){
-                var _age = document.getElementById("age").value;
-                if (document.getElementById("mail").value){
-                    var _mail = document.getElementById("mail").value;	
+    if (document.getElementById("name").value != ''){
+            var _nom = document.getElementById("name").value;
+            if (document.getElementById("goals").value != ''){
+                var _age = document.getElementById("goals").value;
+                if (document.getElementById("pg").value){
+                    var _mail = document.getElementById("pg").value;	
                     var fila="<tr><td>"+_nom+"</td><td>"+_age+"</td><td>"+_mail+"</td><td><input type='image' title='Ver' id='btn_mostrar' src='https://cdn.icon-icons.com/icons2/2406/PNG/512/eye_visible_hide_hidden_show_icon_145988.png' onclick='mostrar();' width='30' height='30'/><input type='image' title='Editar' id='btn_editar' src='https://cdn.icon-icons.com/icons2/1883/PNG/512/twocirclingarrows1_120592.png' onclick='actualizar();' width='30' height='30'/><input type='image' title='Eliminar' id='btn_eliminar' src='https://cdn.icon-icons.com/icons2/868/PNG/512/trash_bin_icon-icons.com_67981.png' onclick='eliminar();' width='30' height='30'/></td>";
                     var btn = document.createElement("TR");
                     btn.innerHTML=fila;
@@ -21,9 +21,9 @@ function guardar(){
         }
     }
     function limpiar(){
-        document.getElementById("nomb").value = "";
-        document.getElementById("age").value = "";
-        document.getElementById("mail").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("goals").value = "";
+        document.getElementById("pg").value = "";
     }
     function eliminar(){
         alert("eliminar registro.")
